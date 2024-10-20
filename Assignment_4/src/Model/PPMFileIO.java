6289536e5abb6268ea2658369b5bd9362977715c
Nakul-27 <nakul27rao@gmail.com>
2024-10-20 15:39:33 -0400

    PPMFileIO saveImage function now works. Issue was that pixel data for PPM files needs to be written as ASCII text, not as binary values. So we needed to write the pixel data to a StringBuilder and then write the bytes of that string to the PPM File.

--- Assignment_4/src/Model/PPMFileIO.java
+++ Assignment_4/src/Model/PPMFileIO.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,15 +20,22 @@ public class PPMFileIO extends AbstractFileIO {
       // TODO: Make this nicer and neater.
       Scanner imageLines = readImage(inputStream);
       token = imageLines.next();
+
       if (!token.equals("P3")) {
-        System.out.println("Invalid PPM file: plain RAW file should begin with P3");
+        throw new IllegalArgumentException("Invalid PPM file: plain RAW file should begin with P3");
       }
+
       int width = imageLines.nextInt();
-      System.out.println("Width of image: "+width);
       int height = imageLines.nextInt();
-      System.out.println("Height of image: "+height);
       int maxValue = imageLines.nextInt();
-      System.out.println("Maximum value of a color in this file (usually 255): "+maxValue);
+
+      if (width <= 0 || height <= 0) {
+        throw new IllegalArgumentException("Invalid image dimensions in PPM file. Width: " + width + " Height: " + height);
+      }
+
+      if (maxValue <= 0) {
+        throw new IllegalArgumentException("Max Value needs to be > 0: " + maxValue);
+      }
 
       int[][][] pixelData = new int[3][height][width];
 
@@ -56,7 +64,9 @@ public class PPMFileIO extends AbstractFileIO {
 
   @Override
   protected void saveImage(Image im, String path) throws IOException {
-    try(OutputStream outputStream = Files.newOutputStream(Paths.get(path))) {
+    try(OutputStream outputStream = new FileOutputStream(path)) {
+      int width = im.getWidth();
+      int height = im.getHeight();
       int[][][] pixels = im.getPixels();
       String header = new StringBuilder("P3\n")
           .append(im.getWidth())
@@ -69,31 +79,34 @@ public class PPMFileIO extends AbstractFileIO {
 
       outputStream.write(header.getBytes());
 
-      for (int y = 0; y < im.getHeight(); ++y) {
-        for (int x = 0; x < im.getWidth(); ++x) {
-          outputStream.write(pixels[0][y][x]);
-          outputStream.write(pixels[1][y][x]);
-          outputStream.write(pixels[2][y][x]);
+      StringBuilder pixelData = new StringBuilder();
+
+      for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < width; ++x) {
+          int red = pixels[0][y][x];
+          int green = pixels[1][y][x];
+          int blue = pixels[2][y][x] ;
+
+          // Write RGB values
+          pixelData.append(red).append(" ")
+                .append(green).append(" ")
+                .append(blue).append(" ");
         }
       }
-    } catch (Exception e) {
-      // TODO: Figure out what exception to throw.
-      throw new IOException("FIGURE OUT WHAT THIS IS.");
+
+      outputStream.write(pixelData.toString().getBytes());
     }
   }
 
   private Scanner readImage(InputStream inputStream) throws IOException {
-    // Don't need to try/catch again beacuse the file was already parsed.
     Scanner sc = new Scanner(inputStream);
     StringBuilder builder = new StringBuilder();
     while (sc.hasNextLine()) {
-      String s = sc.nextLine();
-      if (s.charAt(0) != '#') {
-        builder.append(s + System.lineSeparator());
+      String s = sc.nextLine().split("#")[0].trim();  // Remove inline comments
+      if (!s.isEmpty()) {
+        builder.append(s).append(System.lineSeparator());
       }
     }
-
     return new Scanner(builder.toString());
   }
-
 }
--- Assignment_4/test/Model/PPMFileIOTest.java
+++ Assignment_4/test/Model/PPMFileIOTest.java
@@ -10,22 +10,23 @@ public class PPMFileIOTest {
   @Test
   public void testLoadPPM() {
 
-    Image im = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snail.ascii.ppm");
+    Image im = ImageFactory.loadImage("Assignment_4/files/blackbuck.ascii.ppm");
 
-    assertEquals(256, im.getWidth());
-    assertEquals(256, im.getHeight());
-
+    assertEquals(512, im.getWidth());
+    assertEquals(512, im.getHeight());
   }
 
   @Test
   public void testSavePPM() {
-    Image im1 = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snail.ascii.ppm");
+    Image im1 = ImageFactory.loadImage("Assignment_4/files/blackbuck.ascii.ppm");
 
-    ImageFactory.saveImage(im1, "C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snailSAVED.ascii.ppm");
+    ImageFactory.saveImage(im1, "Assignment_4/files/blackbuck2.ascii.ppm");
 
-    Image im2 = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snailSAVED.ascii.ppm");
+    Image im2 = ImageFactory.loadImage("Assignment_4/files/blackbuck2.ascii.ppm");
 
+    assertEquals(im1.getWidth(), im2.getWidth());
+    assertEquals(im1.getHeight(), im2.getHeight());
+    assertEquals(im1.getMaxValue(), im2.getMaxValue());
     assertEquals(im1.getPixels(), im2.getPixels());
   }
-
 }

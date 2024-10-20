5e44b0b0dc74cba61654e1e2dad5258ec999433e
Nakul-27 <nakul27rao@gmail.com>
2024-10-20 14:49:33 -0400

    Fixed testSavePPM Test to check if the pixelData is the same.

--- Assignment_4/test/Model/PPMFileIOTest.java
+++ Assignment_4/test/Model/PPMFileIOTest.java
@@ -19,9 +19,13 @@ public class PPMFileIOTest {
 
   @Test
   public void testSavePPM() {
-    Image im = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snail.ascii.ppm");
+    Image im1 = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snail.ascii.ppm");
 
-    ImageFactory.saveImage(im, "C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snailSAVED.ascii.ppm");
+    ImageFactory.saveImage(im1, "C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snailSAVED.ascii.ppm");
+
+    Image im2 = ImageFactory.loadImage("C:\\Users\\Shane Holmes\\OneDrive\\Desktop\\Programming Design Paradigms\\Assignment 4\\Assignment_4\\Assignment_4\\files\\snailSAVED.ascii.ppm");
+
+    assertEquals(im1.getPixels(), im2.getPixels());
   }
 
 }
